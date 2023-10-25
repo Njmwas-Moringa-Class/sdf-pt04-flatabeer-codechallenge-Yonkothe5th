4225f6c036fbd0984231303faed704c8a0b2330d
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-26 00:16:22 +0300

    Resubmission of the Code challenge

--- index.js
+++ index.js
@@ -6,29 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const beerDetails = document.querySelector('.beer-details');
 
     function fetchJSON(url, options = {}) {
-        const beerDetails = document.querySelector('.beer-details');
-        // We use the fetch function to send a request to the specified URL.
-        return fetch(url, options)
-        .then(function (response) {
-    // We wait for the response to come back and then convert it to JSON format.
-            return response.json();
+        return fetch(url, options).then(function (response) {
+        return response.json();
     });
     }
 
     function fetchBeers() {
-        fetchJSON(`${baseURL}/beers`).then(function (beers) {
+    return fetchJSON(`${baseURL}/beers`).then(function (beers) {
+        // Clear the beer list before populating it
         beerList.innerHTML = '';
-        beers.forEach(function (beer) {
-        addBeerToList(beer);
-        });
+        for (let i = 0; i < beers.length; i++) {
+        addBeerToList(beers[i]);
+        }
         fetchBeerDetails(currentBeerId);
     });
     }
 
     function fetchBeerDetails(id) {
-        fetchJSON(`${baseURL}/beers/${id}`).then(function (beer) {
+        return fetchJSON(`${baseURL}/beers/${id}`).then(function (beer) {
         displayBeerDetails(beer);
-    });
+        });
     }
 
     function addBeerToList(beer) {
@@ -42,22 +39,62 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function displayBeerDetails(beer) {
-        beerDetails.innerHTML = `
-        <h2>${beer.name}</h2>
-        <img src="${beer.image_url}" alt="${beer.name}" />
-        <p><em>${beer.description}</em></p>
-        <h3>Customer Reviews</h3>
-        <ul id="review-list">
-            ${generateReviewList(beer.reviews)}
-        </ul>
-        `;
+        beerDetails.innerHTML = '<h2>' + beer.name + '</h2>' +
+        '<img src="' + beer.image_url + '" alt="' + beer.name + '" />' +
+        '<p><em>' + beer.description + '</em></p>' +
+        '<form id="description-form">' +
+        '<label for="description">Edited Description:</label>' +
+        '<textarea id="description">' + beer.description + '</textarea>' +
+        '<button type="submit">Update Beer</button>' +
+        '</form>' +
+        '<h3>Customer Reviews</h3>' +
+        '<ul id="review-list">' +
+        generateReviewList(beer.reviews) +
+        '</ul>' +
+        '<form id="review-form">' +
+        '<label for="review">Your Review:</label>' +
+        '<textarea id="review"></textarea>' +
+        '<button type="submit">Add review</button>' +
+        '</form>';
+
+        document.getElementById('description-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const newDescription = document.getElementById('description').value;
+        updateBeer(currentBeerId, { description: newDescription });
+        });
+
+        document.getElementById('review-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const newReview = document.getElementById('review').value;
+        // Use concat to add the new review to the existing reviews
+        updateBeer(currentBeerId, { reviews: beer.reviews.concat(newReview) });
+        });
+
+        document.getElementById('review-list').addEventListener('click', function (event) {
+        if (event.target.tagName === 'LI') {
+        const index = event.target.getAttribute('data-index');
+          // Use slice to remove the clicked review from the array
+        const updatedReviews = beer.reviews.slice(0, index).concat(beer.reviews.slice(index + 1));
+        updateBeer(currentBeerId, { reviews: updatedReviews });
+        }
+    });
+    }
+
+    function updateBeer(id, data) {
+    return fetch(`${baseURL}/beers/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        return response.json();
+    });
     }
 
     function generateReviewList(reviews) {
         let reviewList = '';
-        reviews.forEach(function (review) {
-        reviewList += `<li>${review}</li>`;
-    });
+        for (let i = 0; i < reviews.length; i++) {
+        reviewList += '<li data-index="' + i + '">' + reviews[i] + '</li>';
+        }
         return reviewList;
     }
 
